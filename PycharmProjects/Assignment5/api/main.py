5162a12190f35dc5a4eb4991a8e41dd2bd0ea44c
Danny Semakov <semakovdaniel@gmail.com>
2024-07-18 00:28:23 -0400

    feat: add HTTP requests for sandwiches table

--- PycharmProjects/Assignment5/api/main.py
+++ PycharmProjects/Assignment5/api/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 
 from .models import models, schemas
-from .controllers import orders, order_details
+from .controllers import orders, order_details, sandwiches
 from .dependencies.database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -102,3 +102,47 @@ def delete_one_order_detail(order_detail_id: int,
     if order_detail is None:
         raise HTTPException(status_code=404, detail="User not found")
     return order_details.delete(db=db, order_detail_id=order_detail_id)
+
+
+#HTTP Requests for sandwiches
+@app.post("/sandwiches/", response_model=schemas.Sandwich, tags=["Sandwiches"])
+def create_sandwich(q: schemas.SandwichCreate,
+                    db: Session = Depends(get_db)):
+    return sandwiches.creagit te(db=db, sandwich=sandwich)
+
+
+@app.get("/sandwiches/",
+         response_model=list[schemas.Sandwich],
+         tags=["Sandwiches"])
+def read_sandwiches(db: Session = Depends(get_db)):
+    return sandwiches.read_all(db)
+
+
+@app.get("/sandwiches/{sandwich_id}",
+         response_model=schemas.Sandwich,
+         tags=["Sandwiches"])
+def read_one_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    sandwich = sandwiches.read_one(db, sandwich_id=sandwich_id)
+    if sandwich is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return sandwich
+
+
+@app.put("/sandwiches/{sandwich_id}",
+         response_model=schemas.Sandwich,
+         tags=["Sandwiches"])
+def update_one_sandwich(sandwich_id: int,
+                        sandwich: schemas.SandwichUpdate,
+                        db: Session = Depends(get_db)):
+    sandwich_db = sandwiches.read_one(db, sandwich_id=sandwich_id)
+    if sandwich_db is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return sandwiches.update(db=db, sandwich=sandwich, sandwich_id=sandwich_id)
+
+
+@app.delete("/sandwiches/{sandwich_id}", tags=["Sandwiches"])
+def delete_one_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    sandwich = sandwiches.read_one(db, sandwich_id=sandwich_id)
+    if sandwich is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return sandwiches.delete(db=db, sandwich_id=sandwich_id)
